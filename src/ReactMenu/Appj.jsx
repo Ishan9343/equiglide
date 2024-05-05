579de319e59026dc2eb2c76d7bf715e0dc02b1ff
equiglide <equiglide.in@gmail.com>
2024-05-05 11:40:28 +0530

    changes

--- src/ReactMenu/Appj.jsx
+++ src/ReactMenu/Appj.jsx
@@ -24,7 +24,7 @@ const Appj = () => {
     <>
       <Navbar onFilterChange={handleFilterChange} />
       <Routes>
-        <Route exact path="/equiglide.in" element={<Home />} />
+        <Route exact path="/equiglide" element={<Home />} />
         <Route exact path="/equiglide.in/product" element={<Product filterValue={filterValue} />} />        
         <Route exact path="/about" element={<About />} />
         <Route exact path="/enquiry" element={<Enquiry />} />
